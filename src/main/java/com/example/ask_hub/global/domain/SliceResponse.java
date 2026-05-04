e12d36ac9accc1c44ccbc50ea35e23f2a4e7e921
joeeepark <jaeho4125@gmail.com>
2026-05-05 02:22:51 +0900

    feat: add CommonResponse, SliceResponse

---
+++ src/main/java/com/example/ask_hub/global/domain/SliceResponse.java
@@ -0,0 +1,22 @@
+package com.example.ask_hub.global.domain;
+
+import lombok.Getter;
+import org.springframework.data.domain.Slice;
+
+import java.util.List;
+
+@Getter
+public class SliceResponse<T> {
+
+    private final List<T> content;
+    private final int page;
+    private final int size;
+    private final boolean hasNext;
+
+    public SliceResponse(Slice<T> slice) {
+        this.content = slice.getContent();
+        this.page = slice.getNumber();
+        this.size = slice.getSize();
+        this.hasNext = slice.hasNext();
+    }
+}
