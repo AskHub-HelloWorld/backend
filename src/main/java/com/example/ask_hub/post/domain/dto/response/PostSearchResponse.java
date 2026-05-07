3b64bc9ec650ec5a6cd817027452a2acdf4af535
joeeepark <jaeho4125@gmail.com>
2026-05-07 15:33:48 +0900

    feat: PostSearchResponse DTO

---
+++ src/main/java/com/example/ask_hub/post/domain/dto/response/PostSearchResponse.java
@@ -0,0 +1,36 @@
+package com.example.ask_hub.post.domain.dto.response;
+
+import com.example.ask_hub.post.domain.entity.Post;
+import com.example.ask_hub.user.domain.enums.Position;
+
+import java.time.LocalDateTime;
+
+public record PostSearchResponse(
+        String title,
+        String content,
+        Position position,
+        Integer point,
+        Boolean isResolved,
+
+        String writer, // if anonymous null
+        Boolean isMine,
+        LocalDateTime createdAt,
+        Integer commentCount
+
+) {
+
+    public static PostSearchResponse from(Post post, Long userId, Integer count) {
+        return new PostSearchResponse(
+                post.getTitle(),
+                post.getContent(),
+                post.getPosition(),
+                post.getPoint(),
+                post.getIsResolved(),
+                post.getIsAnonymous() ? null : post.getUser().getName(),
+                post.getUser().getId().equals(userId),
+                post.getCreatedAt(),
+                count
+        );
+    }
+
+}
