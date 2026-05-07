8596e0000522190d74d6e2a0f01e1d1714ae718c
joeeepark <jaeho4125@gmail.com>
2026-05-07 15:34:39 +0900

    chore: PostService

---
+++ src/main/java/com/example/ask_hub/post/application/PostService.java
@@ -0,0 +1,121 @@
+package com.example.ask_hub.post.application;
+
+import com.example.ask_hub.global.domain.SliceResponse;
+import com.example.ask_hub.global.exception.BusinessException;
+import com.example.ask_hub.global.exception.ErrorCode;
+import com.example.ask_hub.post.domain.dto.request.PostCreateRequest;
+import com.example.ask_hub.post.domain.dto.response.PostCategoryResponse;
+import com.example.ask_hub.post.domain.dto.response.PostDetailResponse;
+import com.example.ask_hub.post.domain.dto.response.PostGetResponse;
+import com.example.ask_hub.post.domain.dto.response.PostSearchResponse;
+import com.example.ask_hub.post.domain.entity.Comment;
+import com.example.ask_hub.post.domain.entity.Post;
+import com.example.ask_hub.post.infrastructure.CommentRepository;
+import com.example.ask_hub.post.infrastructure.PostRepository;
+import com.example.ask_hub.user.domain.entity.User;
+import com.example.ask_hub.user.domain.enums.Position;
+import com.example.ask_hub.user.infrastructure.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class PostService {
+
+    private final PostRepository postRepository;
+    private final CommentRepository commentRepository;
+    private final UserRepository userRepository;
+
+
+    public Long create(PostCreateRequest request, Long userId) {
+
+        Optional<Post> existingPost = postRepository.findByTitle(request.title());
+
+        if (existingPost.isPresent()) {
+            throw new BusinessException(ErrorCode.DUPLICATED_TITLE);
+        }
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
+
+        Post post = Post.builder()
+                .title(request.title())
+                .content(request.content())
+                .user(user)
+                .isAnonymous(request.isAnonymous())
+                .point(request.point())
+                .position(request.position())
+                .build();
+
+        postRepository.save(post);
+
+        return post.getId();
+    }
+
+    public SliceResponse<PostGetResponse> getList(Long userId, Pageable pageable) {
+
+        Slice<Post> slice = postRepository.findAll(pageable);
+
+        Slice<PostGetResponse> responses = slice.map(
+                post -> PostGetResponse.from(post, userId, commentRepository.countByPostId(post.getId()))
+        );
+
+        return SliceResponse.from(responses);
+    }
+
+    public PostDetailResponse getDetail(Long postId) {
+
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new BusinessException(ErrorCode.POST_NOT_FOUND));
+
+        return PostDetailResponse.from(post, commentRepository.countByPostId(post.getId()));
+    }
+
+    public Long select(Long postId, Long commentId, Long userId) {
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new BusinessException(ErrorCode.POST_NOT_FOUND));
+
+        if (!post.getUser().getId().equals(user.getId())) {
+            throw new BusinessException(ErrorCode.NOT_ALLOWED_USER);
+        }
+
+        Comment comment = commentRepository.findById(commentId)
+                .orElseThrow(() -> new BusinessException(ErrorCode.COMMENT_NOT_FOUND));
+
+        post.select(comment);
+        return post.getId();
+    }
+
+    public SliceResponse<PostSearchResponse> search(String keyword, Long userId, Pageable pageable) {
+
+        Slice<Post> slice = postRepository.findByTitleContaining(keyword, pageable);
+
+        Slice<PostSearchResponse> responses = slice.map(
+                post -> PostSearchResponse.from(post, userId, commentRepository.countByPostId(post.getId()))
+        );
+
+        return SliceResponse.from(responses);
+    }
+
+    public SliceResponse<PostCategoryResponse> category(Position category, Long userId, Pageable pageable) {
+
+        Slice<Post> slice = postRepository.findAllByPosition(category, pageable);
+
+        Slice<PostCategoryResponse> responses = slice.map(
+                post -> PostCategoryResponse.from(post, userId, commentRepository.countByPostId(post.getId()))
+        );
+
+        return SliceResponse.from(responses);
+    }
+
+
+}
