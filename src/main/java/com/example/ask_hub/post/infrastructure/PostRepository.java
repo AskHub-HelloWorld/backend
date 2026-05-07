a0e2f588175c6b894ffe26eedba4ee6ba5326b7b
joeeepark <jaeho4125@gmail.com>
2026-05-07 15:33:36 +0900

    chore: PostRepository DTO

--- src/main/java/com/example/ask_hub/post/infrastructure/PostRepository.java
+++ src/main/java/com/example/ask_hub/post/infrastructure/PostRepository.java
@@ -1,9 +1,13 @@
 package com.example.ask_hub.post.infrastructure;
 
 import com.example.ask_hub.post.domain.entity.Post;
+import com.example.ask_hub.user.domain.enums.Position;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
@@ -11,4 +15,9 @@ public interface PostRepository extends JpaRepository<Post, Long> {
 
     Integer countByUserId(Long userId);
 
+    Optional<Post> findByTitle(String title);
+
+    Slice<Post> findByTitleContaining(String keyword, Pageable pageable);
+
+    Slice<Post> findAllByPosition(Position category, Pageable pageable);
 }
