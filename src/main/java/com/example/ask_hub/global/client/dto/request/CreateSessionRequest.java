4ebe66ad668f9a845cd847ecd9aebfef09be95e4
joeeepark <jaeho4125@gmail.com>
2026-05-07 19:42:31 +0900

    feat: CreateSessionRequest

--- src/main/java/com/example/ask_hub/global/client/dto/request/CreateSessionRequest.java
+++ src/main/java/com/example/ask_hub/global/client/dto/request/CreateSessionRequest.java
@@ -1,4 +1,10 @@
 package com.example.ask_hub.global.client.dto.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class CreateSessionRequest {
+    private String title;
 }
