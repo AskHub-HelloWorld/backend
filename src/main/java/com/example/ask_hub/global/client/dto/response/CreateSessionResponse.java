4dfc40f5c8cc39a1a5968fbd3c24829351ed18a3
joeeepark <jaeho4125@gmail.com>
2026-05-11 21:44:51 +0900

    refactor: add Json ignore operation

--- src/main/java/com/example/ask_hub/global/client/dto/response/CreateSessionResponse.java
+++ src/main/java/com/example/ask_hub/global/client/dto/response/CreateSessionResponse.java
@@ -1,5 +1,6 @@
 package com.example.ask_hub.global.client.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,6 +9,7 @@ import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class CreateSessionResponse {
     @JsonProperty("session_id")
     private String sessionId;
