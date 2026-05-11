88852173b8fbd21450638c0c8380eecd0ecfc1a9
joeeepark <jaeho4125@gmail.com>
2026-05-11 21:50:41 +0900

    feat: add finding User by List operation

--- src/main/java/com/example/ask_hub/user/infrastructure/UserRepository.java
+++ src/main/java/com/example/ask_hub/user/infrastructure/UserRepository.java
@@ -3,6 +3,8 @@ package com.example.ask_hub.user.infrastructure;
 import com.example.ask_hub.user.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -12,4 +14,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 
     boolean existsByEmail(String email);
+
+
+    List<User> findAllByIdIn(Collection<Long> ids);
 }
