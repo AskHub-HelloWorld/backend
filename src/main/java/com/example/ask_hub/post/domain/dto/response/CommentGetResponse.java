a7bdefce417425727cc305530eca36e31dc455e9
joeeepark <jaeho4125@gmail.com>
2026-05-16 15:22:50 +0900

    feat: Comment API implementation

--- src/main/java/com/example/ask_hub/post/domain/dto/response/CommentGetResponse.java
+++ src/main/java/com/example/ask_hub/post/domain/dto/response/CommentGetResponse.java
@@ -6,6 +6,7 @@ import com.example.ask_hub.user.domain.enums.Position;
 import java.time.LocalDateTime;
 
 public record CommentGetResponse(
+        Long commentId,
         String writer,
         Position position,
         LocalDateTime createdAt,
@@ -14,8 +15,9 @@ public record CommentGetResponse(
 ) {
     public static CommentGetResponse from(Comment comment, Long userId) {
         return new CommentGetResponse(
-                comment.getIsAnonymous() ? null : comment.getUser().getName(),
-                comment.getIsAnonymous() ? null : comment.getUser().getPosition(), // 익명일 때 개발직군 열어둘지?
+                comment.getId(),
+                comment.getIsAnonymous() ? "UNKNOWN" : comment.getUser().getName(),
+                comment.getIsAnonymous() ? Position.UNKNOWN : comment.getUser().getPosition(), // 익명일 때 개발직군 열어둘지?
                 comment.getCreatedAt(),
                 comment.getContent(),
                 comment.getUser().getId().equals(userId)
