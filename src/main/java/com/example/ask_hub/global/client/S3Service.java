807247d6c4bf4a02768a2d4bcc2af864f554fb31
joeeepark <jaeho4125@gmail.com>
2026-05-16 15:29:39 +0900

    refactor: presigned URL

--- src/main/java/com/example/ask_hub/global/client/S3Service.java
+++ src/main/java/com/example/ask_hub/global/client/S3Service.java
@@ -7,9 +7,13 @@ import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
 
 import java.io.IOException;
+import java.time.Duration;
 import java.util.UUID;
 
 @Service
@@ -17,7 +21,7 @@ import java.util.UUID;
 public class S3Service {
 
     private final S3Client s3Client;
-
+    private final S3Presigner s3Presigner;
     @Value("${spring.cloud.aws.s3.bucket}")
     private String bucket;
 
@@ -88,5 +92,21 @@ public class S3Service {
     private String extractKeyFromUrl(String fileUrl) {
 
         return fileUrl.substring(fileUrl.lastIndexOf("/") + 1);
+    }
+
+    public String convertToPresignedUrl(String objectUrl) {
+        String key = extractKeyFromUrl(objectUrl);
+
+        GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                .bucket(bucket)
+                .key(key)
+                .build();
+
+        GetObjectPresignRequest presignRequest = GetObjectPresignRequest.builder()
+                .signatureDuration(Duration.ofMinutes(10))
+                .getObjectRequest(getObjectRequest)
+                .build();
+
+        return s3Presigner.presignGetObject(presignRequest).url().toString();
     }
 }
